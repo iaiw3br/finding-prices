eaaa29e2b81f0655d5fe628d65d34cb45b7c0323
Artem Ivanov <artem.ivanov@onetwotrip.com>
2022-11-27 23:09:21 +0300

    feat: separated search func for each customer

---
+++ internal/customers/pitergsm/search.go
@@ -0,0 +1,24 @@
+package pitergsm
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"strconv"
+	"strings"
+)
+
+func Search(doc *goquery.Document) (float64, error) {
+	var title string
+	// Find the review items
+	doc.Find(".detail-card__price-cur").Each(func(_ int, s *goquery.Selection) {
+		// For each link found, get the title
+		title = s.Find("span").Text()
+		title = strings.ReplaceAll(title, " ", "")
+	})
+
+	price, err := strconv.ParseFloat(title, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return price, nil
+}
