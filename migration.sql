f0de37cc47d08f667e181f3418be14e05c9e8017
Artem Ivanov <artem.ivanov@onetwotrip.com>
2022-11-27 20:43:01 +0300

    refactor: remove unused commands

--- migration.sql
+++ migration.sql
@@ -20,6 +20,8 @@ CREATE TABLE link_items_stores
     FOREIGN KEY (store_id) REFERENCES stores (id),
     url       text
 );
+CREATE INDEX item_id_idx ON link_items_stores (item_id);
+CREATE INDEX store_id_idx ON link_items_stores (item_id);
 
 CREATE TABLE prices
 (
@@ -29,30 +31,4 @@ CREATE TABLE prices
     FOREIGN KEY (item_store_id) REFERENCES link_items_stores (id),
     price         float4
 );
-CREATE INDEX item_id_idx ON link_items_stores (item_id);
-CREATE INDEX store_id_idx ON link_items_stores (item_id);
 CREATE INDEX item_store_idx ON prices (item_store_id);
-
-INSERT INTO items (title)
-VALUES ('Apple MacBook Pro 14" MKGP3 (M1 Pro 8C CPU, 14C GPU, 2021) 16 ГБ, 512 ГБ SSD, серый космос');
-INSERT INTO stores(title, website)
-VALUES ('pitergsm', 'https://pitergsm.ru/');
-INSERT INTO link_items_stores (item_id, store_id, url)
-VALUES (1, 1, 'https://pitergsm.ru/catalog/tablets-and-laptops/mac/macbook-pro/macbook-pro-14-2021/13263/');
-INSERT INTO prices(created, item_store_id, price) VALUES ('2022-11-21', 1, 130490);
-
-INSERT INTO items (title)
-VALUES ('Apple MacBook Pro 14" MKGR3 (M1 Pro 8C CPU, 14C GPU, 2021) 16 ГБ, 512 ГБ SSD, серебристый');
-INSERT INTO link_items_stores (item_id, store_id, url)
-VALUES (2, 1, 'https://pitergsm.ru/catalog/tablets-and-laptops/mac/macbook-pro/macbook-pro-14-2021/13262/');
-INSERT INTO prices(created, item_store_id, price) VALUES ('2022-11-21', 2, 130490);
-
-
-select *
-from prices;
-
-
-drop table prices;
-drop table link_items_stores;
-drop table items;
-drop table stores;
